25857aa2971ef68316e826368b24374ccbacc1f0
Jordan_D <jordanholstdaniels@gmail.com>
2025-09-02 16:50:39 +0100

    styled result component

--- src/components/result/result.jsx
+++ src/components/result/result.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./result.scss";
 import { targetData } from "../../test-data/target-data.js";
-import { sampleData } from "../../test-data/sample-data.js"
+import { sampleData } from "../../test-data/sample-data.js";
+import jokicPic from "../../assets/jokic.jpeg";
 
 
 
@@ -35,35 +36,35 @@ const ResultComponent = () => {
     return (
         <div className="result-container">
             <div id="player-profile">
-                <img id="player-pic"></img>
+                <img id="player-pic" src={jokicPic} alt="Nikola Jokic" ></img>
                 <h1 id="player-name">{sampleData[0].name}</h1>
             </div>
             <div id="result-grid" >
 
                 
                 <div id="draft" className="result-item">
-                    <h1>Draft</h1>
-                    <h1>{sampleData[0].draftYear}</h1>
+                    <h1 className="cell-title">Draft</h1>
+                    <h1 className="cell-data">{sampleData[0].draftYear}</h1>
                 </div>
                 <div id="height" className="result-item">
-                    <h1>Height</h1>
-                    <h1>{sampleData[0].height}</h1>
+                    <h1 className="cell-title">Height</h1>
+                    <h1 className="cell-data">{sampleData[0].height}</h1>
                 </div>
                 <div id="weight" className="result-item">
-                    <h1>Weight</h1>
-                    <h1>{sampleData[0].weight}</h1>
+                    <h1 className="cell-title">Weight</h1>
+                    <h1 className="cell-data">{sampleData[0].weight}</h1>
                 </div>
                 <div id="nationality" className="result-item">
-                    <h1>Nationality</h1>
-                    <h1>{sampleData[0].nationality}</h1>
+                    <h1 className="cell-title">Nationality</h1>
+                    <h1 className="cell-data">{sampleData[0].nationality}</h1>
                 </div>
                 <div id="all-nba" className="result-item">
-                    <h1>All NBA Selections</h1>
-                    <h1>{sampleData[0].allNbaSelections}</h1>
+                    <h1 className="cell-title">All NBA Selections</h1>
+                    <h1 className="cell-data">{sampleData[0].allNbaSelections}</h1>
                 </div>
                 <div id="experience" className="result-item">
-                    <h1>Experience</h1>
-                    <h1>{sampleData[0].experience}</h1>
+                    <h1 className="cell-title">Experience</h1>
+                    <h1 className="cell-data">{sampleData[0].experience}</h1>
                 </div>
             </div>
             <div id="colour-key">
