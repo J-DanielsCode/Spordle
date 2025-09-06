5e7861f92c7473fcaef7f64e7a2f32997fcc3f9e
Jordan_D <jordanholstdaniels@gmail.com>
2025-09-06 03:03:35 +0200

    ability to search from anchor hooks and actually search throught the data

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,23 @@ import ResultComponent from './components/result/result';
  
 function App() {
   const [guessNum, setGuessNum] = useState(1);
+  const [searchResults, setSearchResults] = useState([]);
+  
   return (
     <div className="App">
       <NavBar guessNum={guessNum} setGuessNum={setGuessNum} />
-      <SearchBar guessNum={guessNum} setGuessNum={setGuessNum} />
+      <SearchBar 
+        guessNum={guessNum} 
+        setGuessNum={setGuessNum}
+        setSearchResults={setSearchResults}
+      />
       {/* Only show ResultComponent when guessNum === 2 */}
-      {guessNum > 1 && <ResultComponent guessNum={guessNum} />}
+      {guessNum > 1 && 
+        <ResultComponent 
+          guessNum={guessNum}
+          searchResults={searchResults}
+        />
+      }
     </div>
   );
 }
