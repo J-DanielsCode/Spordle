bb05d1fbab7085084af07b85f83266a65c87eb3a
Jordan_D <jordanholstdaniels@gmail.com>
2025-09-10 01:31:42 +0200

    requests have begun to be written

--- src/helpers/nationalityRequest.js
+++ src/helpers/nationalityRequest.js
@@ -1,28 +1,45 @@
 import { sampleData } from "../test-data/sample-data.js";
 
+export async function fetchNationality(searchResults) {
 
-const query = `
-  query {
-    continent(code: "EU") {
-      name
-      countries {
-        name
-      }
-    }
-  }
-`;
+    const countryName = searchResults.nationality;
 
-fetch("https://countries.trevorblades.com/", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({ query }),
-})
-  .then((res) => res.json())
-  .then((data) => {
-    console.log("Countries in Europe:", data.data.continent.countries);
-  })
-  .catch((err) => {
-    console.error("Error fetching countries:", err);
+    const query = `
+        query {
+            countries(filter: { name: { eq: "${countryName}" } }) {
+                name
+                code
+                continent {
+                    name
+                }
+            }
+        }
+  `;
+
+    fetch("https://countries.trevorblades.com/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ query }),
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log("Countries in Europe:", data.data.continent.countries);
+        })
+        .catch((err) => {
+            console.error("Error fetching countries:", err);
+        });  
+
+    
+  const response = await fetch("https://countries.trevorblades.com/", { // this response needs to only give the continent, to then use to do another query which lists all countries within the continent.
+    //if the target country is in the list then it is close.
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ query }),
   });
+
+  return response.json();
+}
+
+
